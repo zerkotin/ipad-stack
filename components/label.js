d3c86155879efe99caacd7c8c2c2895ca0187d74
Itzik Cohen Arazi <zerkotin@users.noreply.github.com>
2019-06-16 20:01:25 +0200

    added label and button

---
+++ components/label.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Label(props) {
+    return <div className="label">{props.children}</div>;
+}
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {List} from './components/list.js';
+import {Button} from './components/button.js';
+import {Label} from './components/label.js';
 
 function Showcase() {
-    return <List items={['item1', 'item2', 'item3']}/>;
+    return <div className="showcase">
+    	<List items={['item1', 'item2', 'item3']}/>
+    	<Label>I'm a label</Label>
+    	<Button>Click me</Button>
+    </div>;
 }
 
 ReactDOM.render(
